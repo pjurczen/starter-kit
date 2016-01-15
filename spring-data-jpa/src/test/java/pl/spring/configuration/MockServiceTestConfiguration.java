a4a564867f7696045db7d750007a1567cd44a8e9
Piotr Jurczenko <jurczenko.piotr@gmail.com>
2016-01-15 10:28:21 +0100

    configuration add

--- spring-data-jpa/src/test/java/pl/spring/configuration/MockServiceTestConfiguration.java
+++ spring-data-jpa/src/test/java/pl/spring/configuration/MockServiceTestConfiguration.java
@@ -2,9 +2,13 @@ package pl.spring.configuration;
 
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import pl.spring.demo.dao.BookDao;
 
+@Configuration
+@Import(CommonServiceTestConfiguration.class)
 public class MockServiceTestConfiguration {
     
     @Bean
