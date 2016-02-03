65ee4479d94cc898e5011e92c473f3e6a6013b73
Piotr Jurczenko <jurczenko.piotr@gmail.com>
2016-02-03 16:28:57 +0100

    Revert "hgf"
    This reverts commit ffdcfce3a126fef758f5828e07590bd2025799de.

--- spring-data-model/src/main/java/pl/spring/demo/entity/BookEntity.java
+++ spring-data-model/src/main/java/pl/spring/demo/entity/BookEntity.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-@Entity(name = "BOOK")
+@Entity
 @Table(name = "BOOK")
 public class BookEntity implements Serializable {
     /**
